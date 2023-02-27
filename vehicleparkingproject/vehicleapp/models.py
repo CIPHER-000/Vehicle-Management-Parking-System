dabe4bc0422adf4b428339e21e57b07daa881330
Raseena Anwar <raseanwar@gmail.com>
2023-02-27 12:25:26 +0400

    login added

--- vehicleparkingproject/vehicleapp/models.py
+++ vehicleparkingproject/vehicleapp/models.py
@@ -15,8 +15,36 @@ class Vehicles(models.Model):
     color=models.CharField(max_length=10)
     is_active=models.BooleanField()
     entry_date= models.DateTimeField()
-    updated_date=models.DateTimeField()
-    exit_date=models.DateTimeField()
-
+    updated_date=models.DateTimeField(null=True, blank=True)
+    exit_date=models.DateTimeField(null=True, blank=True)
     def __str__(self):
         return self.vehicle_name
+
+class Credit_Cards(models.Model):
+    users_id=models.ForeignKey(Profiles,on_delete=models.CASCADE)
+    card_number=models.IntegerField(10)
+    cardholder_name=models.CharField(max_length=200)
+    expiration_date=models.DateField()
+    is_default=models.BooleanField()
+    created_at=models.DateTimeField()
+    updated_at=models.DateTimeField(null=True, blank=True)
+
+class Parking_Lots(models.Model):
+    name=models.CharField(max_length=200)
+    location=models.CharField(max_length=200)
+    total_spaces=models.IntegerField()
+    available_space=models.IntegerField()
+    is_active=models.BooleanField()
+    created_at=models.BooleanField()
+    updated_at=models.BooleanField(null=True, blank=True)
+class Parking_Reservations(models.Model):
+    users_id=models.ForeignKey(Profiles,on_delete=models.CASCADE)
+    parking_space_id=models.ForeignKey(Parking_Lots,on_delete=models.CASCADE)
+    start_time=models.DateTimeField()
+    end_time=models.DateTimeField()
+    is_active=models.BooleanField()
+    created_at=models.DateTimeField()
+    updated_at=models.DateTimeField(null=True, blank=True)
+    exited_at=models.DateTimeField(null=True, blank=True)
+
+
--- vehicleparkingproject/vehicleapp/views.py
+++ vehicleparkingproject/vehicleapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_protect
+
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request,'base.html')
-def login(requset):
-    return render(requset,'admin_login.html')
+
+@csrf_protect
+def login(request):
+    return render(request, 'admin_login.html')
